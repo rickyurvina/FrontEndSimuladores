40fba945af5a63e8057dbb21904213aecbf09d76
rickyproconty <ricardo.urvina@proconty.com>
2020-11-07 20:42:34 -0500

    Simulador Inversion SisAleman

--- src/app/simulators/simulators.component.ts
+++ src/app/simulators/simulators.component.ts
@@ -14,21 +14,82 @@ export class SimulatorsComponent implements OnInit {
   data:Client[];
   current_clien:Client;
   crud_operation={is_new:false, is_visible:false};
+  amortizacionIA={is_visible:false};
+
 
   constructor(private service: ClientService, private toastr: ToastrService) {
     this.data=[];
    }
+   //Ahorro Flex
   amount: number;
   term: number;
   returnRate: number;
   retention:number
   total: number;
-
+   //Ahorro DPF
   amountDpf: number;
   termDpf: number;
   returnRateDpf: number;
   retentionDpf:number;
   totalDpf: number;
+
+  //Credito Inversión Aleman
+
+
+  tasaInteresAnualIA:number;
+  seguroDesgravamenIA:number;
+  tasaInteresPeriodicaIA:number;
+  valorPrestamoIA:number;
+  tiempoPrestamoIA:number;
+  numeroDePagosPorAnoIA:number;
+  numeroCuotasIA:number;
+  interesDelPeriodoIA:number;
+  capitalAmortizadoIA:number;
+  seguroIA:number;
+  cuotaPagarIA:number;
+  saldoRemanenteIA:number;
+  dataIA=[];
+  sumaIntereses:number;
+
+  vetTablaIA(){
+
+    this.amortizacionIA.is_visible=true;
+
+  }
+
+
+  inversionAleman(): void {
+    this.numeroCuotasIA=this.tiempoPrestamoIA*12;
+    this.capitalAmortizadoIA=this.valorPrestamoIA/this.numeroCuotasIA;
+    this.saldoRemanenteIA=this.valorPrestamoIA;
+    this.tasaInteresAnualIA=13;
+    this.tasaInteresPeriodicaIA=this.tasaInteresAnualIA/12;
+    this.seguroDesgravamenIA=0.0688/100;
+    this.sumaIntereses=0;
+
+    for(let i=0; i<this.numeroCuotasIA;i++){
+
+      this.interesDelPeriodoIA=this.saldoRemanenteIA*this.tasaInteresPeriodicaIA/100;
+      this.seguroIA=this.saldoRemanenteIA*this.seguroDesgravamenIA/12;
+      this.cuotaPagarIA=this.interesDelPeriodoIA+this.capitalAmortizadoIA+this.seguroIA;
+      this.dataIA.push({numeroCuota: i+1,
+        interesPeriodo:this.interesDelPeriodoIA,
+        capitalAmortizado:this.capitalAmortizadoIA,
+        seguro:this.seguroIA,
+        cuotaPagar:this.cuotaPagarIA,
+      saldoRemanente:this.saldoRemanenteIA
+    });
+    this.saldoRemanenteIA=this.saldoRemanenteIA-this.capitalAmortizadoIA;
+    this.sumaIntereses=this.sumaIntereses+this.interesDelPeriodoIA;
+
+    }
+    console.log(this.dataIA);
+
+
+  }
+
+
+
 
   ngOnInit(): void {
   }
@@ -57,17 +118,21 @@ export class SimulatorsComponent implements OnInit {
 
   }
 
-  onCalc(): void {
+  flexSave(): void {
 
     this.returnRate = this.amount * this.term * 3.5 / 360 / 100;
     this.retention= this.returnRate*0.02;
     this.total = this.amount + this.returnRate-this.retention;
   }
-  onCalc2(): void {
+  dpfSave(): void {
     this.returnRateDpf = this.amountDpf * this.termDpf * 5 / 360 / 100 * 30.4167;
     this.retentionDpf= this.returnRateDpf*0.02;
     this.totalDpf = this.amountDpf + this.returnRateDpf-this.retentionDpf;
   }
+
+
+
+
   formatLabel(value: number) {
     if (value >= 1000) {
       return Math.round(value / 1000) + 'd';
