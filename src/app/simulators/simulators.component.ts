29e906ea695ddc481a66f0a905d8d774c5970cc9
rickyproconty <ricardo.urvina@proconty.com>
2020-11-12 11:31:39 -0500

    PDF Credito Educativo SIn estilo

--- src/app/simulators/simulators.component.ts
+++ src/app/simulators/simulators.component.ts
@@ -3,6 +3,22 @@ import { ToastrService } from 'ngx-toastr';
 import { Client } from '../client';
 import { ClientService } from '../client.service';
 import { MatTabGroup } from "@angular/material/tabs";
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+class Product {
+  name: string;
+  tasaAnual: number;
+  tasaPeriodica: number;
+  numeroCuotas: number;
+  cuotaPeriodica: number;
+  totalInteres: number;
+
+}
+
 
 @Component({
   selector: 'app-simulators',
@@ -34,6 +50,9 @@ export class SimulatorsComponent implements OnInit {
 
   constructor(private service: ClientService, private toastr: ToastrService) {
     this.data = [];
+    this.itemS = 0;
+    this.nombreProducto = "Crédito Educativo";
+    // this.downloadPDF();
   }
 
   //*****************************************************
@@ -85,127 +104,128 @@ export class SimulatorsComponent implements OnInit {
 
   /***************************************************** */
 
-/**Variables Creditos para guardar peticiones del API */
-tasaCreditoEducativo:number;
-tasaCreditoInversion:number;
-tasaCreditoInmobiliario:number;
-montoMinCreditoEducativo:number;
-montoMinCreditoInversion:number;
-montoMinCreditoInmobiliario:number;
-montoMaxCreditoEducativo:number;
-montoMaxCreditoInversion:number;
-montoMaxCreditoInmobiliario:number;
-tiempoMinCreditoEducativo:number;
-tiempoMinCreditoInversion:number;
-tiempoMinCreditoInmobiliario:number;
-tiempoMaxCreditoEducativo:number;
-tiempoMaxCreditoInversion:number;
-tiempoMaxCreditoInmobiliario:number;
+  /**Variables Creditos para guardar peticiones del API */
+  tasaCreditoEducativo: number;
+  tasaCreditoInversion: number;
+  tasaCreditoInmobiliario: number;
+  montoMinCreditoEducativo: number;
+  montoMinCreditoInversion: number;
+  montoMinCreditoInmobiliario: number;
+  montoMaxCreditoEducativo: number;
+  montoMaxCreditoInversion: number;
+  montoMaxCreditoInmobiliario: number;
+  tiempoMinCreditoEducativo: number;
+  tiempoMinCreditoInversion: number;
+  tiempoMinCreditoInmobiliario: number;
+  tiempoMaxCreditoEducativo: number;
+  tiempoMaxCreditoInversion: number;
+  tiempoMaxCreditoInmobiliario: number;
 
-/**Variables Ahorros para guardar peticiones del API */
+  /**Variables Ahorros para guardar peticiones del API */
 
-tasaAhorroFlexSave:number;
-tasaAhorroDpf:number;
-tiempoMinAhorroFlexSave:number;
-tiempoMinAhorroDpf:number;
-tiempoMaxAhorroFlexSave:number;
-tiempoMaxAhorroDpf:number;
+  tasaAhorroFlexSave: number;
+  tasaAhorroDpf: number;
+  tiempoMinAhorroFlexSave: number;
+  tiempoMinAhorroDpf: number;
+  tiempoMaxAhorroFlexSave: number;
+  tiempoMaxAhorroDpf: number;
 
-/**Varibles para almacenar las consultas api de credito y ahorro */
-datosFlexSaving=null;
-datosDpfSaving=null;
-datosCreditoEducativo=null;
-datosCreditoInversion=null;
-datosCreditoInmobiliario=null;
+  /**Varibles para almacenar las consultas api de credito y ahorro */
+  datosFlexSaving = null;
+  datosDpfSaving = null;
+  datosCreditoEducativo = null;
+  datosCreditoInversion = null;
+  datosCreditoInmobiliario = null;
 
-porcentajeSD=null;
+  porcentajeSD = null;
+  nombreProducto: string;
 
-ngOnInit(): void {
-  this.service.getFlexSaving().subscribe(
-    (datos)=>{
-      this.datosFlexSaving=datos;
-      for( let x of this.datosFlexSaving){
-        // console.log(x.name);
-        this.tasaAhorroFlexSave=x.rate;
-        this.tiempoMinAhorroFlexSave=x.minimum_time;
-        this.tiempoMaxAhorroFlexSave=x.maximum_time;
-        // console.log(this.tiempoMaxAhorroDpf);
+  ngOnInit(): void {
+    this.service.getFlexSaving().subscribe(
+      (datos) => {
+        this.datosFlexSaving = datos;
+        for (let x of this.datosFlexSaving) {
+          // console.log(x.name);
+          this.tasaAhorroFlexSave = x.rate;
+          this.tiempoMinAhorroFlexSave = x.minimum_time;
+          this.tiempoMaxAhorroFlexSave = x.maximum_time;
+          // console.log(this.tiempoMaxAhorroDpf);
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    },
-    (error)=>{
-      console.log(error);
-    }
-  )
-  this.service.getDpfSaving().subscribe(
-    (datos)=>{
-      this.datosDpfSaving=datos;
-      for( let x of this.datosDpfSaving){
-        // console.log(x.name);
-        this.tasaAhorroDpf=x.rate;
-        this.tiempoMinAhorroDpf=x.minimum_time;
-        this.tiempoMaxAhorroDpf=x.maximum_time;
-        // console.log(this.tiempoMaxAhorroDpf);
+    )
+    this.service.getDpfSaving().subscribe(
+      (datos) => {
+        this.datosDpfSaving = datos;
+        for (let x of this.datosDpfSaving) {
+          // console.log(x.name);
+          this.tasaAhorroDpf = x.rate;
+          this.tiempoMinAhorroDpf = x.minimum_time;
+          this.tiempoMaxAhorroDpf = x.maximum_time;
+          // console.log(this.tiempoMaxAhorroDpf);
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    },
-    (error)=>{
-      console.log(error);
-    }
-  )
-  this.service.getCreditoEducativo().subscribe(
-    (datos)=>{
-      this.datosCreditoEducativo=datos;
-      for( let x of this.datosCreditoEducativo){
-        // console.log(x.name);
-       this.tasaCreditoEducativo=x.tasa;
-       this.montoMinCreditoEducativo=x.montomin;
-       this.montoMaxCreditoEducativo=x.montomax;
-       this.tiempoMinCreditoEducativo=x.tiempomin;
-       this.tiempoMaxCreditoEducativo=x.tiempomax;
+    )
+    this.service.getCreditoEducativo().subscribe(
+      (datos) => {
+        this.datosCreditoEducativo = datos;
+        for (let x of this.datosCreditoEducativo) {
+          // console.log(x.name);
+          this.tasaCreditoEducativo = x.tasa;
+          this.montoMinCreditoEducativo = x.montomin;
+          this.montoMaxCreditoEducativo = x.montomax;
+          this.tiempoMinCreditoEducativo = x.tiempomin;
+          this.tiempoMaxCreditoEducativo = x.tiempomax;
+        }
+        console.log("tiempo min educativo", this.tiempoMinCreditoEducativo);
+      },
+      (error) => {
+        console.log(error);
       }
-      console.log("tiempo min educativo",this.tiempoMinCreditoEducativo);
-    },
-    (error)=>{
-      console.log(error);
-    }
-  )
-  this.service.getCreditoInversion().subscribe(
-    (datos)=>{
-      this.datosCreditoInversion=datos;
-      for( let x of this.datosCreditoInversion){
-        // console.log(x.name);
-       this.tasaCreditoInversion=x.tasa;
-       this.montoMinCreditoInversion=x.montomin;
-       this.montoMaxCreditoInversion=x.montomax;
-       this.tiempoMinCreditoInversion=x.tiempomin;
-       this.tiempoMaxCreditoInversion=x.tiempomax;
+    )
+    this.service.getCreditoInversion().subscribe(
+      (datos) => {
+        this.datosCreditoInversion = datos;
+        for (let x of this.datosCreditoInversion) {
+          // console.log(x.name);
+          this.tasaCreditoInversion = x.tasa;
+          this.montoMinCreditoInversion = x.montomin;
+          this.montoMaxCreditoInversion = x.montomax;
+          this.tiempoMinCreditoInversion = x.tiempomin;
+          this.tiempoMaxCreditoInversion = x.tiempomax;
+        }
+        console.log(this.tiempoMaxCreditoInversion);
+      },
+      (error) => {
+        console.log(error);
       }
-      console.log(this.tiempoMaxCreditoInversion);
-    },
-    (error)=>{
-      console.log(error);
-    }
-  )
-  this.service.getCreditoInmobiliario().subscribe(
-    (datos)=>{
-      this.datosCreditoInmobiliario=datos;
-      for( let x of this.datosCreditoInmobiliario){
-        // console.log(x.name);
-       this.tasaCreditoInmobiliario=x.tasa;
-       this.montoMinCreditoInmobiliario=x.montomin;
-       this.montoMaxCreditoInmobiliario=x.montomax;
-       this.tiempoMinCreditoInmobiliario=x.tiempomin;
-       this.tiempoMaxCreditoInmobiliario=x.tiempomax;
+    )
+    this.service.getCreditoInmobiliario().subscribe(
+      (datos) => {
+        this.datosCreditoInmobiliario = datos;
+        for (let x of this.datosCreditoInmobiliario) {
+          // console.log(x.name);
+          this.tasaCreditoInmobiliario = x.tasa;
+          this.montoMinCreditoInmobiliario = x.montomin;
+          this.montoMaxCreditoInmobiliario = x.montomax;
+          this.tiempoMinCreditoInmobiliario = x.tiempomin;
+          this.tiempoMaxCreditoInmobiliario = x.tiempomax;
+        }
+        console.log(this.tiempoMaxCreditoInmobiliario);
+      },
+      (error) => {
+        console.log(error);
       }
-      console.log(this.tiempoMaxCreditoInmobiliario);
-    },
-    (error)=>{
-      console.log(error);
-    }
-  )
-  this.porcentajeSD = 0.0688;
-  console.log("segurod",this.porcentajeSeguroDesgravamen);
+    )
+    this.porcentajeSD = 0.0688;
+    console.log("segurod", this.porcentajeSeguroDesgravamen);
 
-}
+  }
 
 
   cerrarTablas(): void {
@@ -239,7 +259,7 @@ ngOnInit(): void {
   //Funciones para capturar cambio de pestana
 
   @ViewChild("mattabgroup", { static: false }) mattabgroup: MatTabGroup;
-
+  itemS: number;
   _selectedTabChange(index: number) {
     console.log("_selectTabChange " + index);
     this.limpiarDatos();
@@ -248,6 +268,22 @@ ngOnInit(): void {
     }
     else {
       this.francesa.is_visible = true;
+    }
+    if (index == 0) {
+      this.itemS = 0;
+      this.nombreProducto = "Crédito Educativo";
+    } else if (index == 1) {
+      this.itemS = 1;
+      this.nombreProducto = "Credito Inversion";
+    } else if (index == 2) {
+      this.itemS = 2;
+      this.nombreProducto = "Credito Inmobilirio ";
+    } else if (index == 3) {
+      this.itemS = 3;
+      this.nombreProducto = "Ahorro Flex Save";
+    } else {
+      this.itemS = 4;
+      this.nombreProducto = "Ahorro DPF";
     }
   }
 
@@ -322,8 +358,8 @@ ngOnInit(): void {
       this.toastr.warning('Monto Maximo $30.000 Monto Minimo $5.000 ', 'Monto Fuera de Rango', {
         timeOut: 4500,
       });
-    } else if (this.numeroCuotas > 60 || this.numeroCuotas <6) {
-      this.numeroCuotas= 6;
+    } else if (this.numeroCuotas > 60 || this.numeroCuotas < 6) {
+      this.numeroCuotas = 6;
       this.toastr.warning('Tiempo Maximo 60 Meses, Tiempo Minimo 6 Meses', 'Tiempo Fuera de Rango', {
         timeOut: 4500,
       });
@@ -384,8 +420,8 @@ ngOnInit(): void {
       this.toastr.warning('Monto Maximo $100.000 Monto Minimo $50.000 ', 'Monto Fuera de Rango', {
         timeOut: 4500,
       });
-    } else if (this.numeroCuotas > 244 || this.numeroCuotas<12) {
-      this.numeroCuotas=12;
+    } else if (this.numeroCuotas > 244 || this.numeroCuotas < 12) {
+      this.numeroCuotas = 12;
       this.toastr.warning('Tiempo Maximo 244 Meses, Tiempo Minimo 12 Meses', 'Tiempo Fuera de Rango', {
         timeOut: 4500,
       });
@@ -440,13 +476,13 @@ ngOnInit(): void {
     this.sumaInteresesF = 0;
     this.base = 1 + this.tasaInteresPeriodica / 100;
     this.saldoRemanenteF = this.valorPrestamo;
-  this.cuotaPagarF = ((this.tasaInteresPeriodica / 100) / (1 - (Math.pow(this.base, -this.numeroCuotas))) * this.valorPrestamo);
-    if (this.valorPrestamo > this.montoMaxCreditoEducativo || this.valorPrestamo<this.montoMinCreditoEducativo) {
+    this.cuotaPagarF = ((this.tasaInteresPeriodica / 100) / (1 - (Math.pow(this.base, -this.numeroCuotas))) * this.valorPrestamo);
+    if (this.valorPrestamo > this.montoMaxCreditoEducativo || this.valorPrestamo < this.montoMinCreditoEducativo) {
       this.valorPrestamo = this.montoMinCreditoEducativo;
       this.toastr.warning('Monto Maximo $30.000, Monto Minimo $1000 ', 'Monto Fuera de Rango', {
         timeOut: 4500,
       });
-    } else if (this.numeroCuotas > this.tiempoMaxCreditoEducativo || this.numeroCuotas<this.tiempoMinCreditoEducativo ) {
+    } else if (this.numeroCuotas > this.tiempoMaxCreditoEducativo || this.numeroCuotas < this.tiempoMinCreditoEducativo) {
       this.numeroCuotas = this.tiempoMinCreditoEducativo;
       this.toastr.warning('Tiempo Maximo 48 Meses, Tiempo Minimo 6 Meses', 'Tiempo Fuera de Rango', {
         timeOut: 4500,
@@ -493,39 +529,36 @@ ngOnInit(): void {
   //Funciones Simuladores de Ahorro
 
   flexSave(): void {
-    if(this.term<1 || this.term>365){
+    if (this.term < 1 || this.term > 365) {
       // this.amount=5000;
-      this.term=1;
+      this.term = 1;
       this.toastr.warning('Limites Fuera de Rango ', 'Advertencia', {
         timeOut: 4500,
       });
 
-    }else{
-    this.returnRate = this.amount * this.term * this.tasaAhorroFlexSave / 360 / 100;
-    this.retention = this.returnRate * 0.02;
-    this.total = this.amount + this.returnRate - this.retention;
+    } else {
+      this.returnRate = this.amount * this.term * this.tasaAhorroFlexSave / 360 / 100;
+      this.retention = this.returnRate * 0.02;
+      this.total = this.amount + this.returnRate - this.retention;
     }
 
   }
   dpfSave(): void {
-    if(this.amountDpf<5000 || this.termDpf<6 || this.termDpf >36){
-      this.amountDpf=5000;
-      this.termDpf=6;
+    if (this.amountDpf < 5000 || this.termDpf < 6 || this.termDpf > 36) {
+      this.amountDpf = 5000;
+      this.termDpf = 6;
       this.toastr.warning('Limites Fuera de Rango ', 'Advertencia', {
         timeOut: 4500,
       });
 
-    }else{
-    this.returnRateDpf = this.amountDpf * this.termDpf * this.tasaAhorroDpf / 360 / 100 * 30.4167;
-    this.retentionDpf = this.returnRateDpf * 0.02;
-    this.totalDpf = this.amountDpf + this.returnRateDpf - this.retentionDpf;
+    } else {
+      this.returnRateDpf = this.amountDpf * this.termDpf * this.tasaAhorroDpf / 360 / 100 * 30.4167;
+      this.retentionDpf = this.returnRateDpf * 0.02;
+      this.totalDpf = this.amountDpf + this.returnRateDpf - this.retentionDpf;
     }
   }
 
   /******************************************************************************** */
-
-
-
 
   /************************************** */
   //Funciones para Guardar el formulario de cliente mediante el api
@@ -562,6 +595,134 @@ ngOnInit(): void {
       return Math.round(value / 1000) + 'k';
     }
     return value;
+  }
+
+  generatePDF(action = 'open') {
+
+    if (this.itemS == 0) {
+      let docDefinition = {
+        content: [
+          {
+            text: 'BANCO PROCREDIT',
+            fontSize: 16,
+            alignment: 'center',
+            color: '#da1d2c'
+          },
+
+          {
+            columns: [
+
+              [
+                {
+                  text: `Fecha: ${new Date().toLocaleString()}`,
+                  alignment: 'right'
+                },
+                {
+                  text: `Producto : ${this.nombreProducto}`,
+                  alignment: 'right'
+                }
+              ]
+            ]
+          },
+          {
+            text: 'Detalles Simulacion',
+            style: 'sectionHeader'
+          },
+
+          {
+            table: {
+              layout: 'lightHorizontalLines', // optional
+              headerRows: 1,
+              widths: ['auto', 'auto'],
+              body: [
+                [{ text: 'Monto del Prestamo', bold: true }, `$${this.valorPrestamo.toFixed(2)}`],
+                [{ text: 'Plazo (Meses)', bold: true }, `${this.numeroCuotas}`],
+                [{ text: 'Tasa Interes Periodica', bold: true }, `${this.tasaInteresPeriodica.toFixed(2)}`],
+                [{ text: 'Cuota a Pagar Periodicamente', bold: true }, `$${this.cuotaPagarF.toFixed(2)}`],
+                [{ text: 'Total Interes a Pagar', bold: true }, `$${this.sumaInteresesF.toFixed(2)}`],
+              ]
+            }
+          },
+          {
+            text: 'Tabla de Amortizacion',
+            style: 'sectionHeader'
+          },
+
+          {
+            style: 'tableExample',
+            table: {
+              layout: 'lightHorizontalLines', // optional
+              headerRows: 1,
+              widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto'],
+              body: [
+                [{text:'#Cuotas',style:'tableHeader'}, 'Interes del Periodo', 'Capital Amortizado', 'Seguro', 'Cuota a Pagar', 'Saldo Remanente'],
+                ...this.dataFrances.map(p => ([p.numeroCuota, '$' + p.interesPeriodo.toFixed(2), '$' + p.capitalAmortizado.toFixed(2), '$' + p.seguro.toFixed(2), '$' + p.cuotaPagar.toFixed(2), '$' + p.saldoRemanente.toFixed(2)]))
+
+              ],
+
+            }
+          },
+
+          {
+            text: 'Visita Nuestra Pagina',
+            style: 'sectionHeader'
+          },
+          {
+            columns: [
+              [{ qr: `https://www.bancoprocredit.com.ec/`, fit: '100' }],
+            ]
+          },
+
+        ],
+        styles: {
+          table:{
+            bold:true,
+            fontSize:10,
+            alignment:'center',
+            decorationColor:'red'
+
+          },
+          sectionHeader: {
+            bold: true,
+            decoration: 'underline',
+            fontSize: 14,
+            margin: [0, 15, 0, 15]
+          },
+          header: {
+            fontSize: 18,
+            bold: true,
+            margin: [0, 0, 0, 10]
+          },
+          subheader: {
+            fontSize: 16,
+            bold: true,
+            margin: [0, 10, 0, 5]
+          },
+          tableExample: {
+            margin: [0, 5, 0, 15]
+          },
+          tableOpacityExample: {
+            margin: [0, 5, 0, 15],
+            fillColor: 'blue',
+            fillOpacity: 0.3
+          },
+          tableHeader: {
+            bold: true,
+            fontSize: 13,
+            color: 'red',
+            background:'black'
+          }
+        }
+      };
+      if (action === 'download') {
+        pdfMake.createPdf(docDefinition).download();
+      } else if (action === 'print') {
+        pdfMake.createPdf(docDefinition).print();
+      } else {
+        pdfMake.createPdf(docDefinition).open();
+      }
+    }
+
   }
 
 }
