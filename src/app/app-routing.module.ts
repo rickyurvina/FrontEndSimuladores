055972ec7b79ed3a1567c6d50cfb938900ea9daf
rickyproconty <ricardo.urvina@proconty.com>
2020-12-02 00:04:21 -0500

    Cmabios  enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,15 @@ import { SimulatorsEComponent } from './simulators-e/simulators-e.component';
 import { SimulatorsComponent } from './simulators/simulators.component';
 
 
-const routes:Routes =
-[{ path: 'educativo', component:SimulatorsEComponent},
+const routes:Routes =[
+// [
+  { path: '', component:SimulatorsComponent},
 
+  { path: 'educativo', component:SimulatorsEComponent},
+// {
+//   path: '/educativo',
+//   loadChildren: () => import('./simulators-e/simulators-e.component').then(m => m.SimulatorsEComponent),
+// },
 // {path:'simuladores',component:SimulatorsComponent}
 ];
 
